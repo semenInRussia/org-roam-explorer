320bfecd3c6cd6936c166f9c871791f45905193f
semenInRussia <hrams205@gmail.com>
2023-05-27 23:56:25 +0300

    [fix] if tag wasn't found, was returned DBError, should TagNotFound

--- crates/org_roam_fetch/src/tag.rs
+++ crates/org_roam_fetch/src/tag.rs
@@ -15,7 +15,10 @@ impl Tag {
         sqlx::query_as("SELECT tag FROM tags WHERE tag = $1")
             .bind(add_quotes_around(name))
             .fetch_one(pool).await
-            .map_err(Error::DBError)
+            .map_err(|err| match err {
+                sqlx::error::Error::RowNotFound => Error::TagNotFound,
+                _ => Error::DBError(err),
+            })
     }
 
     pub fn name(&self) -> String {
@@ -39,7 +42,7 @@ impl Tag {
 #[cfg(test)]
 mod tests {
     use crate::connection::default_db_pool;
-    use crate::tag::Tag;
+    use crate::{tag::Tag, result::Error};
 
     #[tokio::test]
     async fn test_tag_name() {
@@ -47,5 +50,12 @@ mod tests {
         let tag = Tag::by_name("physics", &pool).await
             .expect("Error when fetch a tag");
         assert_eq!(tag.name(), "physics");
+    }
+
+    #[tokio::test]
+    async fn test_tag_not_found() {
+        let pool = default_db_pool().await.expect("I can't open the pool");
+        let err = Tag::by_name("stupid id that can't be in db", &pool).await;
+        assert!(matches!(err, Err(Error::TagNotFound)));
     }
 }
